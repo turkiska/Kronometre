293be50acdf8873954242d10ed68a153703274b3
turkiska <152520110+turkiska@users.noreply.github.com>
2024-01-11 15:58:55 +0300

    Add files via upload

---
+++ kronometre.bat
@@ -0,0 +1,13 @@
+@echo off
+cls
+echo Kronometreyi baslatmak icin bir tusa bas...
+pause >nul
+goto sure
+:sure
+cls
+echo Sure basladi!
+timeout /t 30 >nul
+cls
+echo Sure bitti! Yeniden baslatmak icin bir tusa bas...
+pause >nul
+goto sure
